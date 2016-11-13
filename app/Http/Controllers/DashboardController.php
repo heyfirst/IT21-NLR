8e11d777d2aca3e82e731199a8404cf5694fd24d
KANISORN <firstziiz.k@gmail.com>
2016-11-13 21:25:16 +0700

    final #1 dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 
+use App\Repositories\BookingRepositoryInterface;
+use App\Repositories\SectionRepositoryInterface;
+
 class DashboardController extends MainController
 {
     /**
@@ -12,9 +15,11 @@ class DashboardController extends MainController
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(BookingRepositoryInterface $BookingRepository,SectionRepositoryInterface $SectionRepository)
     {
-
+      parent::__construct();
+      $this->SectionRepository = $SectionRepository;
+      $this->BookingRepository = $BookingRepository;
     }
 
     /**
@@ -24,16 +29,27 @@ class DashboardController extends MainController
      */
     public function index()
     {
-      return view('pages.dashboard');
+      $section = $this->BookingRepository->getSection(15);
+
+      array_add($section,'enroll',[
+      '1' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],1),
+      '2' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],2),
+      '3' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],3),
+      '4' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],4),
+      '5' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],5),
+      '6' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],6),
+      '7' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],7),
+      '8' => $this->SectionRepository->getCountEnrollBySection($section['section_id'],8),
+      ]);
+
+      $content = array(
+        'section' => $section
+      );
+      return view('pages.dashboard',$content);
     }
 
     public function indexByUser()
     {
       return view('pages.dashboard');
-    }
-
-    public function abc()
-    {
-        return "6666";
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -74,9 +74,30 @@
       <div class="row">
         <div class="col-xs-8">
           <div class="panel panel-info">
-            <div class="panel-heading">Enroll Peak Time</div>
+            <div class="panel-heading">Peak Section</div>
             <div class="panel-body">
-
+              <div class="row">
+                <div class="col-xs-4 text-right">
+                  <h3>ROUND {{ $section['section_id'] }}</h3>
+                  <h4 class="time">START <span>{{ date('g:i A', strtotime($section['start_time'])) }}</span></h4>
+                  <h4 class="time">END <span>{{ date('g:i A', strtotime($section['end_time'])) }}</span></h4>
+                  <a href={{ url('/booking/'.$section['section_id']) }} class="btn btn-info btn-block">SEE SECTION</a>
+                </div>
+                <div class="col-xs-3">
+                  @foreach ($section['enroll'] as $key => $enroll)
+                  <div class="progress progress-striped active">
+                    <div class="progress-bar {{ ($enroll>0 && $enroll<40) ? "progress-bar-primary":"" }}{{ ($enroll>40 && $enroll<80) ? "progress-bar-warning":"" }}{{ ($enroll>80 && $enroll<110) ? "progress-bar-danger":"" }}"
+                      role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: {{$enroll}}%;">
+                    </div>
+                  </div>
+                  @endforeach
+                </div>
+                <div class="col-xs-5">
+                  <h3>เต็มภายในเวลา 00:00:00</h3>
+                  <h4 class="time">ตั้งแต่เวลา <span>{{ date('g:i A', strtotime($section['start_time'])) }}</span></h4>
+                  <h4 class="time">ถึงเวลา <span>{{ date('g:i A', strtotime($section['end_time'])) }}</span></h4>
+                </div>
+              </div>
             </div>
           </div>
         </div>
