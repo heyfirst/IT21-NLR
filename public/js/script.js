cf9de93aea903c0003f13c02b289f5c4c1e8d32b
KANISORN <firstziiz.k@gmail.com>
2016-11-13 21:14:17 +0700

    init dashboard incomplete

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
   $('[data-toggle="tooltip"]').tooltip()
-  
+
   $('.sections').mixItUp({
     layout: {
         display: 'block'
@@ -87,6 +87,62 @@ $(document).ready(function(){
 
       });
     });
+  });
+
+  var ctx = document.getElementById("timeline-chart-noon");
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+    labels: ["06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00","15:00","16:00","17:00"],
+    datasets: [{
+            label: "จำนวนคนลงทะเบียนช่วงเช้า",
+            fill: false,
+            lineTension: 0.2,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+        }]
+    },
+  });
+
+  var ctx = document.getElementById("timeline-chart-night");
+  var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+    labels: ["18:00","19:00", "20:00", "21:00", "22:00", "23:00", "00:00", "01:00", "02:00", "03:00","04:00","05:00"],
+    datasets: [{
+            label: "จำนวนคนลงทะเบียนช่วงเย็น",
+            fill: false,
+            lineTension: 0.2,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40,65, 59, 80, 81, 56, 55, 40],
+        }]
+    },
   });
 
 });
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,43 +2,103 @@
 
 @section('content')
   <div class="container dashboard">
-    <div class="row">
-      {{-- Dashboard --}}
-      <div class="col-sm-3">
-        <div class="panel">
-          <div class="panel-body text-right">
-            <h2>Dashboard</h2>
-            <h5 class="subtitle">Network Lab Reservation</h5>
+    <div class="col-sm-10 col-sm-offset-1">
+      {{-- Row 1 --}}
+      <div class="row">
+        {{-- จำนวน Section --}}
+        <div class="col-sm-4">
+          <div class="panel panel-info">
+            <div class="panel-heading">SECTIONs</div>
+            <div class="panel-body text-right">
+              <h2>10</h2>
+            </div>
+          </div>
+        </div>
+        {{-- จำนวน User --}}
+        <div class="col-sm-4">
+          <div class="panel panel-info">
+            <div class="panel-heading">USERs</div>
+            <div class="panel-body text-right">
+              <h2>10</h2>
+            </div>
+          </div>
+        </div>
+        {{-- จำนวนการ Enroll --}}
+        <div class="col-sm-4">
+          <div class="panel panel-info">
+            <div class="panel-heading">ENROLLs</div>
+            <div class="panel-body text-right">
+              <h2>10</h2>
+            </div>
           </div>
         </div>
       </div>
-      {{-- จำนวน Section --}}
-      <div class="col-sm-3">
-        <div class="panel">
-          <div class="panel-body text-right">
-            <h2>10</h2>
-            <h5 class="subtitle">Sections</h5>
+      {{-- Row 2 --}}
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="panel panel-info">
+            <div class="panel-heading">Enroll Peak Time</div>
+              <ul id="myTab1" class="nav nav-tabs nav-justified">
+                <li class="active"><a href="#timeline-noon" data-toggle="tab">Noon</a></li>
+                <li><a href="#timeline-night" data-toggle="tab">Night</a></li>
+              </ul>
+              <div id="myTabContent" class="tab-content">
+              <div class="tab-pane fade active in" id="timeline-noon">
+                <canvas id="timeline-chart-noon" width="400" height="250"></canvas>
+              </div>
+              <div class="tab-pane fade" id="timeline-night">
+                <canvas id="timeline-chart-night" width="400" height="250"></canvas>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="panel panel-info">
+            <div class="panel-heading">Top Enroll User</div>
+            <ul class="list-group">
+              <li class="list-group-item">Cras justo odio<span class="badge badge-warning pull-right">1st</span></li>
+              <li class="list-group-item">Dapibus ac facilisis in<span class="badge badge-info pull-right">2nd</span></li>
+              <li class="list-group-item">Morbi leo risus<span class="badge badge-primary pull-right">3rd</span></li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
         </div>
       </div>
-      {{-- จำนวน User --}}
-      <div class="col-sm-3">
-        <div class="panel">
-          <div class="panel-body text-right">
-            <h2>10</h2>
-            <h5 class="subtitle">Users</h5>
+      {{-- Row 3 --}}
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="panel panel-info">
+            <div class="panel-heading">Enroll Peak Time</div>
+            <div class="panel-body">
+
+            </div>
           </div>
         </div>
-      </div>
-      {{-- จำนวนการ Enroll --}}
-      <div class="col-sm-3">
-        <div class="panel">
-          <div class="panel-body text-right">
-            <h2>10</h2>
-            <h5 class="subtitle">Enrolls</h5>
+        <div class="col-sm-4">
+          <div class="panel panel-info">
+            <div class="panel-heading">Bottom Enroll User</div>
+            <ul class="list-group">
+              <li class="list-group-item">Cras justo odio<span class="badge badge-warning pull-right">1st</span></li>
+              <li class="list-group-item">Dapibus ac facilisis in<span class="badge badge-info pull-right">2nd</span></li>
+              <li class="list-group-item">Morbi leo risus<span class="badge badge-primary pull-right">3rd</span></li>
+              <li class="list-group-item">Porta ac consectetur ac</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+              <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
   </div>
+  <script type="text/javascript" src="/vendor/Chart.min.js"></script>
 @endsection
